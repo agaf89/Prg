b148de3d7dbe33ee2ad10d70ebfa877151083057
Stanislav <agafon_stas89@mail.ru>
2020-06-27 13:58:19 +0300

    SecondCommit

--- javascript.js
+++ javascript.js
@@ -1,8 +1,12 @@
 "use strict";
-
-
 let numberOfFilms = +prompt("Сколько фильмов ты смотрел?", "");
-
+if (numberOfFilms<10){
+    alert("Просмторенно довольно мало фильмов");
+} else if (numberOfFilms<30){
+    alert("Вы классический зритель");
+} else {
+    alert("Вы киноман");
+}
 
 let personalMovieDB = {
     count:  numberOfFilms,
@@ -11,13 +15,25 @@ let personalMovieDB = {
     genres: [],
     privat: false  
 };
+let i = 0;
+while (i!=2){
+    let a = prompt('Один из последних просмотренных фильмов?', '');
+    if (a==""){
+        continue;
+    } else if (a==false){
+        continue;
+    } else if (a.length>50){
+        continue;
+    }
+    let b = prompt('На сколько оцените его?', '');
+    if (b==""){
+        continue;
+    } else if (b==false){
+        continue;
+    }
+    personalMovieDB.movies[a] = b;
+    i++;
+}
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
